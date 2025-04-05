4b6c1868f9f4ad2b1bcc4e169283a47abaa7e5f8
Balaji-11-udayasuriyan <balajiudayasuriyan1102@gmail.com>
2025-04-05 12:49:30 +0530

    Update views.py

--- frontend/views.py
+++ frontend/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
-# Create your views here.
+import os
+from django.shortcuts import render
+from dotenv import load_dotenv
+import requests
+import json
+
+load_dotenv()  # take environment variables
+
+# Access the variables
+API_KEY = os.getenv('API_KEY')
+
+def home(request):
+
+    if request.method == 'POST':
+
+        location = request.POST['location']
+
+        print(location)
+
+        url = f"http://api.weatherstack.com/current?access_key={API_KEY}&query=" + location
+
+        weather_request = requests.get(url)
+        weather_api = json.loads(weather_request.text)
+
+        print(weather_api)
+        return render(request, "frontend/home.html", {'weather_api': weather_api})
+
+    else:
+
+        url = f"http://api.weatherstack.com/current?access_key={API_KEY}&query=New%20York"
+
+        weather_request = requests.get(url)
+        weather_api = json.loads(weather_request.text)
+
+        return render(request, "frontend/home.html", {'weather_api': weather_api})
